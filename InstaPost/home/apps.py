44fe7997244b559052c1bdddb96b66b81c3ad898
shihabsaleem <shihabchikkushihab@gmail.com>
2024-01-10 19:05:12 +0530

    app home

--- InstaPost/home/apps.py
+++ InstaPost/home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CredsConfig(AppConfig):
+class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'creds'
+    name = 'home'
